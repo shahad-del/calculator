ba6131096881babfe5b43af6a16830034a72651d
Shahad Suman <shahadsuman@Shahads-MacBook-Air.local>
2021-09-13 22:40:23 +0545

    calculator through number input

---
+++ calculator.js
@@ -0,0 +1,80 @@
+let operand1 = 0;
+let operand2 = 0;
+let operator = undefined;
+let shouldReplaceNextDigit = false;
+
+function readNumber(keyVal) {
+  const displayInput = document.getElementById("userNumber");
+  let curVal = displayInput.value;
+  if (curVal === "0" || curVal === "") {
+    displayInput.value = keyVal;
+  } else {
+    if (keyVal === ".") {
+      if (curVal.indexOf(".") != -1) {
+        return;
+      }
+
+      shouldReplaceNextDigit = true;
+
+      displayInput.value += ".0";
+    } else {
+      if (shouldReplaceNextDigit) {
+        var idx = curVal.indexOf(".") + 1;
+        //curVal[idx] = keyVal;
+        curVal = curVal.substring(0, idx) + keyVal;
+        shouldReplaceNextDigit = false;
+        displayInput.value = curVal;
+      }else {
+        displayInput.value += keyVal;
+      }
+    }
+  }
+}
+
+
+function readOperator(opVal) {
+  const displayInput = document.getElementById("userNumber");
+  const curVal = displayInput.value;
+  if (curVal === "0" || curVal === "") {
+    return;
+  } else {
+    operand1 = parseFloat(curVal);
+    operator = opVal;
+    displayInput.value = "";
+    document.getElementById("output").innerText = curVal + " " + opVal;
+  }
+}
+
+function calcResult() {
+  if (operand1 === 0) return;
+  if (operator === undefined) return;
+  const displayInput = document.getElementById("userNumber");
+  const curVal = displayInput.value;
+  if (curVal === "0" || curVal === "") {
+    return;
+  } else {
+    operand2 = parseFloat(curVal);
+    let result = 0;
+    switch (operator) {
+      case "+":
+        result = operand1 + operand2;
+        break;
+      case "-":
+        result = operand1 - operand2;
+        break;
+      case "*":
+        result = operand1 * operand2;
+        break;
+      case "/":
+        result = operand1 / operand2;
+        break;
+    }
+
+    displayInput.value = "";
+    document.getElementById("output").innerText =
+      operand1 + " " + operator + " " + operand2 + " = " + result;
+    operand1 = 0;
+    operand2 = 0;
+    operator = undefined;
+  }
+}
