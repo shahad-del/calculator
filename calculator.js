e30f9f5035d6551c5249e501160baeb33dc76383
Shahad Suman <shahadsuman@Shahads-MacBook-Air.local>
2021-09-14 08:13:45 +0545

    Handled input non-numeric key blocking

--- calculator.js
+++ calculator.js
@@ -3,34 +3,49 @@ let operand2 = 0;
 let operator = undefined;
 let shouldReplaceNextDigit = false;
 
-function readNumber(keyVal) {
-  const displayInput = document.getElementById("userNumber");
-  let curVal = displayInput.value;
-  if (curVal === "0" || curVal === "") {
-    displayInput.value = keyVal;
-  } else {
-    if (keyVal === ".") {
-      if (curVal.indexOf(".") != -1) {
-        return;
-      }
+// function readNumber(keyVal) {
+//   const displayInput = document.getElementById("userNumber");
+//   let curVal = displayInput.value;
+//   if (curVal === "0" || curVal === "") {
+//     displayInput.value = keyVal;
+//   } else {
+//     if (keyVal === ".") {
+//       if (curVal.indexOf(".") != -1) {
+//         return;
+//       }
 
-      shouldReplaceNextDigit = true;
+//       shouldReplaceNextDigit = true;
 
-      displayInput.value += ".0";
-    } else {
-      if (shouldReplaceNextDigit) {
-        var idx = curVal.indexOf(".") + 1;
-        //curVal[idx] = keyVal;
-        curVal = curVal.substring(0, idx) + keyVal;
-        shouldReplaceNextDigit = false;
-        displayInput.value = curVal;
-      }else {
-        displayInput.value += keyVal;
-      }
+//       displayInput.value += ".0";
+//     } else {
+//       if (shouldReplaceNextDigit) {
+//         var idx = curVal.indexOf(".") + 1;
+//         //curVal[idx] = keyVal;
+//         curVal = curVal.substring(0, idx) + keyVal;
+//         shouldReplaceNextDigit = false;
+//         displayInput.value = curVal;
+//       }else {
+//         displayInput.value += keyVal;
+//       }
+//     }
+//   }
+// }
+function checkIfDigit(event) {
+  const curKey = event.key;
+  const curKeyCode = event.keyCode;
+  const curKeyAsInt = parseInt(curKey);
+
+  console.log(curKeyCode);
+  var allowedKeys = [8, 16, 37, 38, 39, 40,46, 190];
+
+  var isSpecialKeyPressed = event.metaKey || event.ctrlKey;
+  if (!isSpecialKeyPressed && allowedKeys.indexOf(curKeyCode) == -1) {
+    if (isNaN(curKeyAsInt)) {
+      event.preventDefault();
+      return;
     }
-  }
+  } 
 }
-
 
 function readOperator(opVal) {
   const displayInput = document.getElementById("userNumber");
